594575e3662a18f0c19884e47376f7ed1beaf2ca
Awesome-SSP <142079519+Awesome-SSP@users.noreply.github.com>
2025-11-05 19:24:46 +0530

    refactor: improve formatting and indentation in ServiceWorkerRegistration component

--- components/service-worker-registration.tsx
+++ components/service-worker-registration.tsx
@@ -3,18 +3,18 @@
 import { useEffect } from 'react'
 
 export default function ServiceWorkerRegistration() {
-  useEffect(() => {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker
-        .register('/service-worker.js')
-        .then((registration) => {
-          console.log('Service Worker registered successfully:', registration)
-        })
-        .catch((error) => {
-          console.log('Service Worker registration failed:', error)
-        })
-    }
-  }, [])
+    useEffect(() => {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker
+                .register('/service-worker.js')
+                .then((registration) => {
+                    console.log('Service Worker registered successfully:', registration)
+                })
+                .catch((error) => {
+                    console.log('Service Worker registration failed:', error)
+                })
+        }
+    }, [])
 
-  return null
+    return null
 }
